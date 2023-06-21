40b2c54e3ae150673dd7f702ff93f13d3b1d0a6f
fezz02 <federicopalcich02@gmail.com>
2023-06-21 11:34:40 +0200

    new file:   .scribe/.filehashes 	new file:   .scribe/auth.md 	new file:   .scribe/endpoints.cache/00.yaml 	new file:   .scribe/endpoints.cache/01.yaml 	new file:   .scribe/endpoints.cache/02.yaml 	new file:   .scribe/endpoints/00.yaml     new file:   .scribe/endpoints/01.yaml 	new file:   .scribe/endpoints/02.yaml 	new file:   .scribe/endpoints/custom.0.yaml 	new file:   .scribe/intro.md 	modified:   app/Http/Controllers/Api/V1/Auth/LoginController.php 	modified:   app/Http/Controllers/Api/V1/TourController.php 	modified:   app/Http/Controllers/Api/V1/TravelController.php 	modified:   composer.json 	modified:   composer.lock 	new file:   config/scribe.php 	new file:   public/docs/collection.json 	new file:   public/docs/css/theme-default.print.css 	new file:   public/docs/css/theme-default.style.css 	new file:   public/docs/images/navbar.png 	new file:   public/docs/index.html 	new file:   public/docs/js/theme-default-4.21.2.js 	new file:   public/docs/js/tryitout-4.21.2.js 	new file:   public/docs/openapi.yaml

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -11,6 +11,15 @@ use App\Services\TravelService;
 
 class TravelController extends Controller
 {
+    /**
+     * GET Travels
+     *
+     * Returns paginated list of travels.
+     *
+     * @queryParam page integer Page number. Example: 1
+     *
+     * @response {"data":[{"id":"9958e389-5edf-48eb-8ecd-e058985cf3ce","name":"First travel", ...}}
+     */
     public function index()
     {
         $travels = Travel::query()
@@ -20,6 +29,16 @@ class TravelController extends Controller
         return TravelResource::collection($travels);
     }
 
+    /**
+     * POST Travel
+     *
+     * Creates a new Travel record.
+     *
+     * @authenticated
+     *
+     * @response {"data":{"id":"996a36ca-2693-4901-9c55-7136e68d81d5","name":"My new travel 234","slug":"my-new-travel-234", ...}
+     * @response 422 {"message":"The name has already been taken.","errors":{"name":["The name has already been taken."]}}
+     */
     public function store(StoreTravelRequest $request, TravelService $service)
     {
         $travel = $service->store($request->validated());
@@ -27,6 +46,16 @@ class TravelController extends Controller
         return response()->json(TravelResource::make($travel), 200);
     }
 
+    /**
+     * PUT Travel
+     *
+     * Updates new Travel record.
+     *
+     * @authenticated
+     *
+     * @response {"data":{"id":"996a36ca-2693-4901-9c55-7136e68d81d5","name":"My new travel 234", ...}
+     * @response 422 {"message":"The name has already been taken.","errors":{"name":["The name has already been taken."]}}
+     */
     public function update(UpdateTravelRequest $request, Travel $travel, TravelService $service)
     {
         $travel = $service->update($request->validated(), $travel);
